712ae95c3b3453c602766ae730d4acda5de8be27
Michael Watters <michael.watters@dart.biz>
2018-08-15 16:11:40 -0400

    Add new pic

--- app.py
+++ app.py
@@ -16,13 +16,15 @@ def hello():
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
     img_url = 'https://tr4.cbsistatic.com/hub/i/r/2017/10/13/1455bd12-7a96-48ee-8ad2-7af33d31017c/thumbnail/768x432/843ec3e4805e49358b2c9dc52100b373/20171010dockerjack.jpg'
+    img2_url = 'https://blogs.gartner.com/richard-watson/files/2015/05/Worked-Fine-In-Dev-Ops-Problem-Now.jpg'
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}" \
            "<p><img src='{img_url}' alt='I <3 Docker'/></p>" \
+           "<p><img src='{img2_url}' alt='Let it all burn'/></p>"
 
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits, img_url=img_url)
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits, img_url=img_url, img2_url=img2_url)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
